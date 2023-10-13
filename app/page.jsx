5cadec28fdd6e4cc3a69fd0bf5c2d2df00cdb8cc
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-10-13 22:29:14 +0100

    created heading

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,17 @@
 const Home = () => {
   return (
-    <div>page</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient text-center">AI Powered Prompts</span>
+            <p className="desc text-center">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus omnis animi autem quos sed, sequi ratione dolore possimus impedit ullam. Id, iure nesciunt saepe numquam consequuntur ratione cum doloribus consectetur!
+            </p>
+
+            {/* feed */}
+        </h1>
+    </section>
   )
 }
 
