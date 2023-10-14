0f35b67f8cb9411eaac3b85dbc44e3d7f0965b5e
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-10-14 07:59:06 +0100

    newly created components

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,20 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center">
-            Discover & Share
-            <br className="max-md:hidden"/>
-            <span className="orange_gradient text-center">AI Powered Prompts</span>
-            <p className="desc text-center">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus omnis animi autem quos sed, sequi ratione dolore possimus impedit ullam. Id, iure nesciunt saepe numquam consequuntur ratione cum doloribus consectetur!
-            </p>
-
-            {/* feed */}
-        </h1>
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI Powered Prompts</span>
+        <p className="desc text-center">
+          AI Prompt is an open source prompting tool for modern world to
+          discover, create and share creative prompts.
+        </p>
+        <Feed />
+      </h1>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
