bdf142e8b16b47204a4e6e6b387b66d8e33b6e42
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-10-14 21:30:16 +0100

    mobie nav set-up

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,14 +6,109 @@ import { useState, useEffect } from "react"
 import { signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
+    const isUserLoggedIn = true;
+
+    const [providers, setProviders] = useState(null);
+
+    const [ toggleDropdown, setToggleDropdown] = useState(false);
+
+    useEffect(() => {
+        const setProviders = async () => {
+            const response = await getProviders();
+
+            setProviders(response)
+        }
+    }, [])
+
   return (
-    <nav className='flex-between w-full mb-2 pt-3'>
-        <Link href="/" className='flex gap-2 flex-center'/>
-        <Image src="/assets.images/logo.svg"
+    <nav className='flex-between w-full mb-16 pt-3'>
+        <Link href="/" className='flex gap-2 flex-center'>
+        <Image src="/assets/images/logo.svg"
         alt='prompt-image'
         width={30}
         height={30}
         className='object-contain' />
+
+        <p className='logo_text'>
+            AI Prompt
+        </p>
+        </Link>
+
+        {/* Desktop nav */}
+        <div className='sm:flex hidden'>
+            {isUserLoggedIn ? (
+                <div className='flex gap-3 md:gap-5'>
+                    <Link href="/create-prompt" className='black_btn'>
+                        Create Post 
+                    </Link>
+
+                    <button type="button" onClick={signOut} className='outline_btn'>
+                        Sign Out
+                    </button>
+
+                    <Link href="/profile">
+                        <Image src="/assets/images/human-avatar.jpg" 
+                        width={37} 
+                        height={37}
+                        className='rounded-full'
+                        alt='profile'
+                        />
+                    </Link>
+                </div>
+            ):(
+                <div>
+                    { providers && Object.values(providers.map((provider) => (
+                        <button
+                            type='button'
+                            key={ provider.name }
+                            onClick={() => signIn (providers.id)}
+                            className='black_btn'
+                        >
+                            Sign In
+
+                        </button>
+                    )))}
+                </div>
+            )}
+        </div>
+
+        {/* mobile nav */}
+        <div className='sm:hidden flex relative'>
+            {isUserLoggedIn ? (
+                <div className='flex'>
+                                            <Image src="/assets/images/human-avatar.jpg" 
+                        width={37} 
+                        height={37}
+                        className='rounded-full'
+                        alt='profile'
+                        onClick={() => setToggleDropdown((prev) => !prev) }
+                        />
+
+                        {toggleDropdown && (
+                            <div className='dropdown'>
+                                <Link href="/profile"
+                                className='dropdown_link' onClick={() => setToggleDropdown(false)}>
+                                    My Profile
+                                </Link>
+                                </div>
+                        )}
+                </div>
+            ):(
+                <div>
+                    { providers && Object.values(providers.map((provider) => (
+                        <button
+                            type='button'
+                            key={ provider.name }
+                            onClick={() => signIn (providers.id)}
+                            className='black_btn'
+                        >
+                            Sign In
+
+                        </button>
+                    )))}
+                </div>
+            )}
+        </div>
     </nav>
   )
 }
