55522d2f18e65c0e02bf79a8782b785accb90964
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-10-16 12:14:15 +0100

    changes made to feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Feed = () => {
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat similique incidunt corporis soluta nemo porro dignissimos explicabo rerum. Voluptatibus molestias esse a debitis nihil maiores in aperiam iusto porro assumenda.
+      Feed
     </div>
   )
 }
